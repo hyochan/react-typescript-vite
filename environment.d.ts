771d0accce5a656b7c7573f7a3ff1cfc3943180a
hyochan <dooboolab@gmail.com>
2022-09-11 00:51:42 +0900

    feat: support svg

--- environment.d.ts
+++ environment.d.ts
@@ -17,3 +17,8 @@ declare module '*.svg' {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
 }
+
+declare module '*.svg?component' {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,12 @@
 import {VitePWA} from 'vite-plugin-pwa';
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from '@honkhonk/vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    svgr(),
     react(),
     // babel({
     //   babelConfig: {
